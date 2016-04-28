6ced377dc4503ff0a838ebb987b12e2ea9641895
MitchGianoni <mitchgianoni@yahoo.com>
2016-04-27 20:17:57 -0400

    Update api class for review..

--- app/services/yahoo_api.rb
+++ app/services/yahoo_api.rb
@@ -77,8 +77,6 @@ class YahooApi
 		encoded = Base64.encode64("#{client_id}:#{secret_id}")
 		{
 			"Authorization" => "Basic #{encoded}",
-			"Content-Type"  => "application/x-www-form-urlencoded",
-			"Accept" => "application/x-www-form-urlencoded",
 		}
 	end
 end
