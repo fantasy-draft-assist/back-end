93926ccad76786f73490cba44579f9174650b165
MitchGianoni <mitchgianoni@yahoo.com>
2016-04-29 14:04:11 -0400

    trying something new

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -12,7 +12,7 @@ class OauthController < ApplicationController
 		@user = User.find(params[:user_id])
 		@api = YahooApi.new(@user)
 		@api.oauth_get_token(code)
-		render json: { user: @user.as_json(only: [:y_access_token, :y_refresh_token]) },
+		render json: { user: @user.as_json(only: [:y_access_token, :y_refresh_token, :y_token_type, :y_expires_at]) },
 		  status: :ok
 	end
 end
