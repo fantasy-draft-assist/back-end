265383abbf866a74f3447b7a517fc2325cfb76f7
MitchGianoni <mitchgianoni@yahoo.com>
2016-04-28 23:30:28 -0400

    added logger

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,6 +3,7 @@ class OauthController < ApplicationController
 	def new
 		@user = User.find(params[:user_id])
 		@api = YahooApi.new(@user)
+		Rails.logger.warning"request url:#{@api.oauth_request_url}"
 		redirect_to @api.oauth_request_url
 	end
 
