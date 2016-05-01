2191c6bcb52691289ea5a1e43495581799dd7c5a
MitchGianoni <mitchgianoni@yahoo.com>
2016-05-01 14:59:33 -0400

    learning

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -6,11 +6,12 @@ class PlayerController < ApplicationController
 
 	def jagr
 
-		jagr = URI("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20fantasysports.players.stats%20where%20league_key%3D'352.l.641'%20and%20player_key%3D'352.p.35'&format=json&diagnostics=true&callback=")
+		jagr = URI("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20fantasysports.players.stats%20where%20league_key%3D'352.l.641'%20and%20player_key%3D'352.p.35&format=json&diagnostics=true&callback=")
 
 		@user = User.find(params[:user_id])
+		Rails.logger.warn "The token is: #{@user.y_access_token}"
 		request = Net::HTTP::Get.new(jagr)
-		request["Authorization"] = "#{@user.y_token_type} #{@user.y_access_token}"
+		request["Authorization"] = "Bearer #{@user.y_access_token}"
 
 		Rails.logger.warn "Request: #{request}"
 
