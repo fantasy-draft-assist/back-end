14065c889190d3c852d6ff10d88fc4a1c36dcc43
MitchGianoni <mitchgianoni@yahoo.com>
2016-05-02 11:38:38 -0400

    player key game key life key

--- app/services/yahoo_api.rb
+++ app/services/yahoo_api.rb
@@ -18,8 +18,8 @@ class YahooApi
 	# kinda like this: 
 	# HTTParty.get("path/for/fantasy/sports/thing", query: whatever, headers: @headers)
 
-	def get_player_stats(player_key)
-		url = URI("https://fantasysports.yahooapis.com/fantasy/v2/player/#{player_key}/stats?format=json")
+	def get_player_stats(game_key, letter, player_key)
+		url = URI("https://fantasysports.yahooapis.com/fantasy/v2/player/#{game_key}.p.#{player_key}/stats?format=json")
 		http = Net::HTTP.new(url.host, url.port)
 		http.use_ssl = true
 		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
