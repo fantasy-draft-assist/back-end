773bb15941b19c14bff2e043b4ccdf463947a737
MitchGianoni <mitchgianoni@yahoo.com>
2016-05-02 10:56:25 -0400

    Get player stats for an individual player.

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -7,7 +7,8 @@ class PlayerController < ApplicationController
 	def jagr
 		@user = User.find(params[:user_id])
 
-		url = URI("https://fantasysports.yahooapis.com/fantasy/v2/players;player_keys=352.p.35?format=json")
+		url = URI("http://fantasysports.yahooapis.com/fantasy/v2/player/352.p.35/stats?format=json")
+		# url = URI("https://fantasysports.yahooapis.com/fantasy/v2/players;player_keys=352.p.35?format=json")
 		http = Net::HTTP.new(url.host, url.port)
 		http.use_ssl = true
 		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
