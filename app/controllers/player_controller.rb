5b070d319dd84930586c26efd1967881e15fff90
MitchGianoni <mitchgianoni@yahoo.com>
2016-05-02 10:47:02 -0400

    Actually have an SSL url when we're trying to do SSL.

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -5,18 +5,16 @@ class PlayerController < ApplicationController
 
 
 	def jagr
-
-		url = URI("http://fantasysports.yahooapis.com/fantasy/v2/players;player_keys=352.p.35&format=json&diagnostics=true&callback=")
-
 		@user = User.find(params[:user_id])
 
+		url = URI("https://fantasysports.yahooapis.com/fantasy/v2/players;player_keys=352.p.35&format=json&diagnostics=true&callback=")
 		http = Net::HTTP.new(url.host, url.port)
 		http.use_ssl = true
 		http.verify_mode = OpenSSL::SSL::VERIFY_NONE
 		request = Net::HTTP::Get.new(url)
 		request["Authorization"] = "#{@user.y_access_token}"
 		request["cache-control"] = 'no-cache'
-	  	request["content-type"] = 'application/x-www-form-urlencoded'
+	    request["content-type"] = 'application/x-www-form-urlencoded'
 		response = http.request(request)
 
 		render json: { headers: response, body: response.body }
