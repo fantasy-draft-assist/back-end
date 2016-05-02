42e11b2ffba6f5b0797e3b4dfac3439f86f60612
MitchGianoni <mitchgianoni@yahoo.com>
2016-05-02 10:40:49 -0400

    Say your fuckin prayers.

--- app/controllers/player_controller.rb
+++ app/controllers/player_controller.rb
@@ -6,10 +6,9 @@ class PlayerController < ApplicationController
 
 	def jagr
 
-		url = URI("https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20fantasysports.players.stats%20where%20league_key%3D'352.l.641'%20and%20player_key%3D'352.p.35'&format=json&diagnostics=true&callback=")
+		url = URI("https://fantasysports.yahooapis.com/fantasy/v2?q=select%20*%20from%20fantasysports.players.stats%20where%20league_key%3D'352.l.641'%20and%20player_key%3D'352.p.35'&format=json&diagnostics=true&callback=")
 
 		@user = User.find(params[:user_id])
-		Rails.logger.warn "The token is: #{@user.y_access_token}"
 
 		http = Net::HTTP.new(url.host, url.port)
 		http.use_ssl = true
@@ -19,8 +18,6 @@ class PlayerController < ApplicationController
 		request["cache-control"] = 'no-cache'
 	  	request["content-type"] = 'application/x-www-form-urlencoded'
 		response = http.request(request)
-
-		Rails.logger.warn "Request: #{request}"
 
 		render json: { headers: response, body: response.body }
 	end
