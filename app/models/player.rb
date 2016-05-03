8d6ac4a3c29b4d42740fed7fe83a19b08dbb25b7
MitchGianoni <mitchgianoni@yahoo.com>
2016-05-03 16:38:10 -0400

    create vs new

--- app/models/player.rb
+++ app/models/player.rb
@@ -19,11 +19,10 @@ class Player < ActiveRecord::Base
 			pro_team = ProTeam.find_or_create_by(name: team_data["name"],
 				                                 abbreviation: team_data["abbreviation"],
 				                                 yahoo_team_id: team_data["yahoo_team_id"])
-			pro_player = player.pro_players.new(player_id: player.id,
-												pro_team_id: pro_team.id,
-				                                season: data["pro_player"]["season"])
-			stats = pro_player.player_stats.new(data["stats"])
-			player.save
+			pro_player = player.pro_players.create(player_id: player.id,
+												   pro_team_id: pro_team.id,
+				                                   season: data["pro_player"]["season"])
+			stats = pro_player.player_stats.create(data["stats"])
 		end
 	end
 end
