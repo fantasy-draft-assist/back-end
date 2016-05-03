b06fcd6504fcc58fea6ccd6672d7e2f9d4ce7efb
MitchGianoni <mitchgianoni@yahoo.com>
2016-05-03 14:58:41 -0400

    added missin comma

--- app/models/player.rb
+++ app/models/player.rb
@@ -19,7 +19,7 @@ class Player < ActiveRecord::Base
 			pro_team = ProTeam.find_or_create_by(name: team_data["name"],
 				                                 abbreviation: team_data["abbreviation"],
 				                                 yahoo_team_id: team_data["yahoo_id"])
-			pro_player = player.pro_players.new(player_id: player.id
+			pro_player = player.pro_players.new(player_id: player.id,
 												pro_team_id: pro_team.id,
 				                                season: data["pro_player"]["season"])
 			stats = player.stats.new(data["stats"])
