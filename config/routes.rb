d8149dc6e0b1db158f64ba18b6e9889b2dd94751
MitchGianoni <mitchgianoni@yahoo.com>
2016-05-08 14:02:13 -0400

    added route for all season stats

--- config/routes.rb
+++ config/routes.rb
@@ -17,9 +17,10 @@ Rails.application.routes.draw do
   get 'oauth/:user_id/authorize', to: 'oauth#create', as: 'authorize'
 
   ### METHODS TO FROND END
-  
+
   # one player data
-  get 'players/:yahoo_player_id/:season', to: 'players#one', as: 'player'
+  get 'players/:yahoo_player_id/:season', to: 'players#one', as: 'player_season'
+  get 'players/:yahoo_player_id', to 'players#one_all_seasons', as: 'player'
   # all players data 
   get 'players/index', to: 'players#index'
   get 'players/index/:season', to: 'players#season'
