4601d8aeaad95848ee7bd00c3299248b5765ae66
MitchGianoni <mitchgianoni@yahoo.com>
2016-05-09 15:31:22 -0400

    added functionality to search players by first or last name

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
 	def player_search
-    @players = Player.all
+    @players = Player.includes(:pro_players)
     @players = @players.fname(params[:first_name]) if params[:first_name].present?
-    @players = @players.lname(params[:last_mame]) if params[:last_name].present?
+    @players = @players.lname(params[:last_name]) if params[:last_name].present?
+    binding.pry
     render json: @players
   end
 end
