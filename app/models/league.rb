7ab20bda6d0b1b5958ad9441facf416f7bb9f37e
MitchGianoni <mitchgianoni@yahoo.com>
2016-05-09 15:32:59 -0400

    added validation so that user id is always present, removed uniqueness validation from league name

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,5 +2,6 @@ class League < ActiveRecord::Base
 	belongs_to :user
 	has_many :fantasy_teams
 
-	validates :name, presence: true, uniqueness: true
+	validates :name, presence: true
+	validates :user_id, presence: true
 end
