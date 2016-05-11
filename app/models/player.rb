8476d261e4f39e7e8049c73af5529eb98b7796bb
MitchGianoni <mitchgianoni@yahoo.com>
2016-05-11 15:52:43 -0400

    added dependent: :destroys to reduce database

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
 class Player < ActiveRecord::Base
-	has_many :pro_players
-	has_many :pro_teams, through: :pro_players
+	has_many :pro_players, dependent: :destroy
+	has_many :pro_teams, through: :pro_players, dependent: :destroy
 	has_many :player_stats, through: :pro_players, dependent: :destroy
 
 	include PgSearch
