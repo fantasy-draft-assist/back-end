0ea40f2f8884c8454c2a693649fb11db9337f3c3
MitchGianoni <mitchgianoni@yahoo.com>
2016-05-11 01:41:23 -0400

    removed binding.pry

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,6 @@ class SearchController < ApplicationController
     @players = Player.includes(:pro_players)
     @players = @players.fname(params[:first_name]) if params[:first_name].present?
     @players = @players.lname(params[:last_name]) if params[:last_name].present?
-    binding.pry
     render json: @players
   end
 end
